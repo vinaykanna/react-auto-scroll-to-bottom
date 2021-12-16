c53ad591a96434948c05e1528e2e4438c58ceb1f
vinaykanna <48582080+vinaykanna@users.noreply.github.com>
2021-12-16 11:40:33 +0530

    apply padding

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ function App() {
 
   return (
     <div style={{ display: "flex", gap: 20, maxWidth: 800, margin: "auto", marginTop:100 }}>
-      <ScrollToBottom height={300} style={{ marginTop: 50, border:"1px solid rgba(0,0,0,0.1)" }}>
+      <ScrollToBottom height={300} style={{ marginTop: 50, border:"1px solid rgba(0,0,0,0.1)", padding: "0px 20px" }}>
         {list.map((item) => (
           <p>{item}</p>
         ))}
